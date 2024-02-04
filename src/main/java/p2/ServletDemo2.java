0358b096c09abc109c62e50d9c850f0f7b8c248d
gautham1024 <perigautamanudeep123@gmail.com>
2024-02-04 16:03:02 +0530

    this is my first commit

---
+++ src/main/java/p2/ServletDemo2.java
@@ -0,0 +1,71 @@
+package p2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDemo2
+ */
+@WebServlet("/ServletDemo2")
+public class ServletDemo2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDemo2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("fname");
+		String email = request.getParameter("mail");
+		String pass = request.getParameter("pwd");
+		
+		try {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo1","root","tiger");
+		PreparedStatement ps = con.prepareStatement("insert into webdemo1 values(?,?,?)");
+		ps.setString(1, name);
+		ps.setString(2, email);
+		ps.setString(3, pass);
+		ps.execute();
+		con.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
